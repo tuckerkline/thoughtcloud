413733a74484e039db6c48abfc56e2458aa8a769
tuckerkline <tucker.kline@gmail.com>
2015-11-20 12:29:37 -0700

    adding thoughts and comments works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,62 @@
 angular.module('thoughtApp', ['ngRoute'])
 
-angular.module('sandwichApp')
+angular.module('thoughtApp')
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/', {
-	            templateUrl : '/html/login.html',
+	            templateUrl : '/html/home.html',
 	            controller  : 'mainController'
 	            })
+			.when('/thoughts', {
+				templateUrl : '/html/thoughts.html',
+				controller  : 'thoughtsController'
+				})
 
 
-	            	}])
+	    }])
 
-angular.module('sandwichApp')
-	.controller('mainController', ['$scope', '$rootScope', '$http', '$location', 'authService', function($scope, $rootScope, $http, $location, authService) {
+angular.module('thoughtApp')
+	.controller('mainController', ['$scope', '$rootScope', '$http', '$location', function($scope, $rootScope, $http, $location) {
+
+		$scope.submitThought = function() {
+			console.log('hi')
+			$http.post('/thoughtAdd', $scope.newThought)
+                .then(function(returnData){
+                    console.log(returnData)
+                })
+
+		}
+}])
 
 
-	}])
+angular.module('thoughtApp')
+	.controller('thoughtsController', ['$scope', '$rootScope', '$http', '$location', function($scope, $rootScope, $http, $location) {
+
+		$http({
+            method : 'GET',
+            url    : '/allthoughts',
+        }).then(function(returnData) {
+            console.log(returnData.data)
+            $scope.thoughts = returnData.data
+        })
+
+        console.log($scope.thoughts)
+
+
+        $scope.addComment = function(thought) {
+
+        	thought.comments.push(thought.addedComment)
+        	$http({
+                method : 'POST',
+                url    : '/commentadd',
+                data   : thought
+            }).then(function(returnData) {
+                // no need for anything here
+            })
+        	console.log(thought.thought)
+
+        	thought.addedComment = ""
+        }
+
+
+}])
